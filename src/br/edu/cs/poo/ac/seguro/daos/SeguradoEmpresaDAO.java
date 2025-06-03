3738c09197ef26a0c2cec4163e6aafdd6b2ab051
Marina Hoffmann Guimarães <167432472+marinaghoffmann@users.noreply.github.com>
2025-06-02 23:21:42 -0300

    melhoria 3

--- src/br/edu/cs/poo/ac/seguro/daos/SeguradoEmpresaDAO.java
+++ src/br/edu/cs/poo/ac/seguro/daos/SeguradoEmpresaDAO.java
@@ -8,4 +8,20 @@ public class SeguradoEmpresaDAO extends DAOGenerico<SeguradoEmpresa> {
     protected Class<SeguradoEmpresa> getClasseEntidade() {
         return SeguradoEmpresa.class;
     }
+
+    public SeguradoEmpresa buscar(String cnpj) {
+        return (SeguradoEmpresa) super.buscar(cnpj);
+    }
+
+    public boolean incluir(SeguradoEmpresa empresa) {
+        return super.incluir(empresa);
+    }
+
+    public boolean alterar(SeguradoEmpresa empresa) {
+        return super.alterar(empresa);
+    }
+
+    public boolean excluir(String cnpj) {
+        return super.excluir(cnpj);
+    }
 }
--- src/br/edu/cs/poo/ac/seguro/daos/SeguradoPessoaDAO.java
+++ src/br/edu/cs/poo/ac/seguro/daos/SeguradoPessoaDAO.java
@@ -9,4 +9,21 @@ public class SeguradoPessoaDAO extends DAOGenerico<SeguradoPessoa> {
     protected Class<SeguradoPessoa> getClasseEntidade() {
         return SeguradoPessoa.class;
     }
+
+    public SeguradoPessoa buscar(String cpf) {
+        return (SeguradoPessoa) super.buscar(cpf);
+    }
+
+    public boolean incluir(SeguradoPessoa pessoa) {
+        return super.incluir(pessoa);
+    }
+
+    public boolean alterar(SeguradoPessoa pessoa) {
+        return super.alterar(pessoa);
+    }
+
+    public boolean excluir(String cpf) {
+        return super.excluir(cpf);
+    }
+
 }
